ed7a55e21cf6f5ca50bcd5f5ad3c4213b605e9c8
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-24 14:14:19 +0530

    Pooja Added: Validating Mobile Number

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -7,13 +7,13 @@ namespace UserRegistrationRegex
 {
     class UserRegistration
     {
-        public void validateEmail(string email)
+        public void validateMobileNo(string mobileno)
         {
-            string stringForEmail = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
-            if (Regex.IsMatch(email, stringForEmail))
-                Console.WriteLine(email + " is Valid");
+            string stringForMobileNo = "^[0-9]{2}[ ][6-9][0-9]{9}$";
+            if (Regex.IsMatch(mobileno, stringForMobileNo))
+                Console.WriteLine(mobileno + " is Valid");
             else
-                Console.WriteLine(email + " is Invalid");
+                Console.WriteLine(mobileno + " is Invalid");
         }
     }
 }
