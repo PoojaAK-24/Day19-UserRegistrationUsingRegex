71f35dd49f6805a39d8414d36dd84093d08356bd
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-24 18:19:01 +0530

    Pooja Added: Validating All Sample Email Id's

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -7,14 +7,16 @@ namespace UserRegistrationRegex
 {
     class UserRegistration
     {
-        public void validateSpecialCharPassword(string specialcharpassword)
+        public static string EMAIL_REGEX = "^[A-Z0-9a-z]+([+-.#$^*][0-9]+)?[0-9]*[@][A-Za-z0-9]+[.][A-Za-z]{2,}([.][a-zA-Z]{2,})?$";
+        public void validateAllTypeEmail(string email)
         {
-            string stringForSpecialCharPassword = "^([a-zA-Z0-9])*[!@#$%^&*]{1}([a-zA-Z0-9])*$";
-            if (Regex.IsMatch(specialcharpassword, stringForSpecialCharPassword))
-                Console.WriteLine(specialcharpassword + " is Valid");
+            if (Regex.IsMatch(email, EMAIL_REGEX))
+                Console.WriteLine(email + " is Valid");
             else
-                Console.WriteLine(specialcharpassword + " is Invalid");
+                Console.WriteLine(email + " is Invalid");
+
         }
     }
 }
+
 
