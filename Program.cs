c03698c0845f58d017b4e2ea149ab76216b95837
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-24 13:30:39 +0530

    Pooja Added: Validating First Name

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserRegistrationRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            Console.WriteLine("*First letter should be capital.");
+            Console.WriteLine("*length should be Minimum three characters.");
+            UserRegistration validateUser = new UserRegistration();
+            validateUser.validateFirstName("Pooja");
+           
+            Console.ReadKey();
+        }
+    }
+}
