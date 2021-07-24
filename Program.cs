aee03aed6635c87e34961c6e8f27c9f0f78e7e0a
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-24 14:55:18 +0530

    Pooja Added: Validating Password

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace UserRegistrationRegex
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration");
-            Console.WriteLine("*Mobile Number should be starts from Country Code follow by space and 10 digit number");
+            Console.WriteLine("*Password should contain minimum 8 Characters");
             UserRegistration validateUser = new UserRegistration();
-            validateUser.validateMobileNo("91 8770422433");
+            validateUser.validatePassword("pooja@123");
            
             Console.ReadKey();
         }
