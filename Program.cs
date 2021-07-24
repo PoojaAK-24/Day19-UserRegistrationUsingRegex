ed18b0b2854bbfa70db96122cc9e808e5a8879a9
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-24 18:18:21 +0530

    Pooja Added: Validating All Sample Email Id's

--- Program.cs
+++ Program.cs
@@ -8,10 +8,17 @@ namespace UserRegistrationRegex
         {
             Console.WriteLine("Welcome to User Registration");
             Console.WriteLine("*Password should contain minimum 8 Characters");
-            Console.WriteLine("Password should contain numeric values");
             UserRegistration validateUser = new UserRegistration();
-            validateUser.validateSpecialCharPassword("reddy#123");
            
+            validateUser.validateAllTypeEmail("pooja@yahoo.com");
+            validateUser.validateAllTypeEmail("pooja-100@yahoo.com");
+            validateUser.validateAllTypeEmail("abc.100@yahoo.com");
+            validateUser.validateAllTypeEmail("abc111@abc.com");
+            validateUser.validateAllTypeEmail("abc-100@abc.net");
+            validateUser.validateAllTypeEmail("abc-100@abc.com.ac");
+            validateUser.validateAllTypeEmail("abc@1.com");
+            validateUser.validateAllTypeEmail("abc@gmail.com.com");
+            validateUser.validateAllTypeEmail("abc+100@gmail.com");
             Console.ReadKey();
         }
     }
